ac617d12d9fc69ce88024ce636237f7e1a666350
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-01-30 14:02:29 -0600

    I added more products

--- main.js
+++ main.js
@@ -68,6 +68,31 @@ productList.push({
     price: 5200,
     image: "https://cdn.shopify.com/s/files/1/0622/0660/7617/products/ZoneSGreen1.jpg?v=1652314624",
 });
+productList.push ({
+    name:'Sunglasses',
+    price: 30,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 42,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
+productList.push ({
+    name:'Sunglasses',
+    price: 40,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 110,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
 /*             <div class="product-card">
                 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
                 <div class="product-info">
